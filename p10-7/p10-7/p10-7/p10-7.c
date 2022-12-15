9a50182881cbe6ccc01b4cd41301b85c236535de
ntut111360206 <a0975754743a@gmail.com>
2022-12-15 19:52:28 +0800

    HW6

---
+++ p10-7/p10-7/p10-7/p10-7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct card
+{
+	char *face;
+	char *suit;
+};
+int main()
+{
+	struct card acard;
+	struct card *cardptr;
+	acard.face = "Ace";
+	acard.suit = "Spades";
+	cardptr = &acard;
+	printf("%s%s%s\n%s%s%s\n%s%s%s\n", acard.face, " of ", acard.suit, 
+		cardptr->face, " of ", cardptr->suit, (*cardptr).face, " of ", (*cardptr).suit);
+	system("pause");
+	return 0;
+}
